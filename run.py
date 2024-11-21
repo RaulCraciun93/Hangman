9dfa99bd51e0a83fa28e251ae4bd07462d6c68c6
Raul Craciun <raul.carciun@students.codeinstitute.net>
2024-11-21 15:01:34 +0000

    Set up word bank and added main function with welcome message

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Import the random module to select a random word
+"""
+import random
+
+"""
+Word bank for the game,
+This list contains the words that our game will randomly choose from.
+"""
+WORD_BANK = ["python", "hangman", "game", "programming", "project", "developer"]
+
+def main():
+    """
+    Main function to run the Hangman game.
+    """
+    print("Welcome to Hangman!")
+    print("Game is starting. Stay tuned!")
+
+main()
